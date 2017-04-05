433c8b4ed03156ae7e3eee8570dc3dbd7abd4943
knaquin <knaquin@radiantblue.com>
2017-04-05 12:45:48 -0400

    Testing...

--- yml.sh
+++ yml.sh
@@ -7,23 +7,23 @@ echo "- Home: index.md" >> mkdocs.yml
 for app in ${APPS[@]} ; do
 
     # if the app has any documentation at all, add it to the yml
-    if [ -d "docs/$app/docs" ]; then
+    if [ -d "$SCRIPT_DIR/docs/$app/docs" ]; then
   		    echo "- $app:" >> mkdocs.yml
 
             # if the app has a build guide, add it to the yml
-            BUILD_GUIDE=$app/docs/build-guide.md
+            BUILD_GUIDE=$SCRIPT_DIR/$app/docs/build-guide.md
             if [ -e docs/$BUILD_GUIDE ]; then
                 echo "  - Build Guide: $BUILD_GUIDE" >> mkdocs.yml
             fi
 
             # if the app has an install guide, add it to the yml
-            INSTALL_GUIDE=$app/docs/install-guide.md
+            INSTALL_GUIDE=$SCRIPT_DIR/$app/docs/install-guide.md
             if [ -e docs/$INSTALL_GUIDE ]; then
                 echo "  - Install Guide: $INSTALL_GUIDE" >> mkdocs.yml
             fi
 
             # if the app has a user guide, add it to the yml
-            USER_GUIDE=$app/docs/user-guide.md
+            USER_GUIDE=$SCRIPT_DIR/$app/docs/user-guide.md
             if [ -e docs/$USER_GUIDE ]; then
                 echo "  - User Guide: $USER_GUIDE" >> mkdocs.yml
             fi
