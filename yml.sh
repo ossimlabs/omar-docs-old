4ef66ac26e078efe0397f0f1cf38561fd24cfb65
knaquin <kevin.naquin@digitalglobe.com>
2017-06-27 08:23:03 -0400

    Just using the text for main version.

--- yml.sh
+++ yml.sh
@@ -3,7 +3,7 @@
 echo "site_name: O2 Web Services" > mkdocs.yml
 echo "theme: windmill" >> mkdocs.yml
 echo "extra:" >> mkdocs.yml
-echo "  version: 2.0.0 Daytona Beach" >> mkdocs.yml
+echo "  version: Daytona Beach" >> mkdocs.yml
 echo "pages:" >> mkdocs.yml
 echo "- Home: index.md" >> mkdocs.yml
 
