694d270664cbd8ac32abf1bc4525fc2563798a05
knaquin <kevin.naquin@digitalglobe.com>
2017-06-27 08:22:37 -0400

    Taking out build guide.

--- home-page.sh
+++ home-page.sh
@@ -2,8 +2,8 @@
 
 pushd docs
 
-echo "| App | Guides |   |   |" >> index.md
-echo "| --- | ------ | - | - |" >> index.md
+echo "| App |   |   |" >> index.md
+echo "| --- | - | - |" >> index.md
 
 for repo in ${REPOS[@]} ; do
 
