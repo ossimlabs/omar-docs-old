9cbf48e910b4c22a320ecacf4d4998d888188274
Tran <ktran@radiantblue.com>
2018-03-07 16:36:17 -0500

    updated to work on mac

--- docs/format_release.sh
+++ docs/format_release.sh
@@ -3,16 +3,16 @@
 #This script takes out unnecessary Confluence tokens from the copied and pasted sprint's Confluence Change log page
 
 if [[ $# -lt 1 ]]; then
-	sed -i 's/[0-9]*\ issues*\ \ Refresh//g' index.md
-	sed -i 's/Change\ Log//' index.md
-	sed -i 's/No\ issues\ found\ \ Refresh//g' index.md
+	sed -i'.bak' 's/[0-9]*\ issues*\ \ Refresh//g' index.md
+	sed -i'.bak' 's/Change\ Log//' index.md
+	sed -i'.bak' 's/No\ issues\ found\ \ Refresh//g' index.md
 
 
 
 else
-	sed -i 's/[0-9]*\ issues*\ \ Refresh//g' ${1}
-	sed -i 's/Change\ Log//' ${1}
-	sed -i 's/No\ issues\ found\ \ Refresh//g' ${1}
+	sed -i'.bak' 's/[0-9]*\ issues*\ \ Refresh//g' ${1}
+	sed -i'.bak' 's/Change\ Log//' ${1}
+	sed -i'.bak''s/No\ issues\ found\ \ Refresh//g' ${1}
 fi
 
 
