a21831ebbe9d9a305e1025674563f189a95af6ab
knaquin <kevin.naquin@digitalglobe.com>
2019-05-23 13:24:10 -0400

    Testing...

--- functions.sh
+++ functions.sh
@@ -7,6 +7,7 @@ function getRepoVersion {
     REPO=$2
 
     for x in `find $REPO -name gradle.properties | head -1`; do
+	echo "$x"
         eval $1=`grep buildVersion $x | awk -F "=" '{print $2}'`
     done
 }
