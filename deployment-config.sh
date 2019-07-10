328a1e322fdfe1c1e1ae2a6ddf00e0c8e4025ecf
anaga670 <andrew.nagawiecki@radiantsolutions.com>
2019-07-10 15:53:49 -0400

    Updated deploymentConfig format

--- deployment-config.sh
+++ deployment-config.sh
@@ -13,17 +13,20 @@ pushd deployment_configs
 
 for repo in ${REPOS[@]} ; do
     app=`echo $repo | sed -n 's/.*[/]\(.*\).git$/\1/p'`
+
+    if [[ $app == "omar-base" ]]; then
+        continue
+    fi
     echo "Current App is $app"
+
     for deploymentConfig in `ls`; do
-        echo "Current Deployment Config is $deploymentConfig"
         if [[ $deploymentConfig == *"$app"* ]]; then
             GUIDE=$SCRIPT_DIR/docs/$app/docs/install-guide/$app.md
-            echo "Found deploymentConfig for $app"
             # only modify the guide if it exists and a deployment config exists
             if [ -e $GUIDE ]; then
-                echo "Found install-guide for $app"
+                echo "Found deploymentConfig for $app"
                 echo "" >> $GUIDE # make sure you start on a new line
-                echo "## Example OpenShift Deployment Config." >> $GUIDE
+                echo "## Example OpenShift Deployment Config" >> $GUIDE
                 echo "\`\`\`" >> $GUIDE
                 cat $deploymentConfig >> $GUIDE
                 echo  "\`\`\`" >> $GUIDE
